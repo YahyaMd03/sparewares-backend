0c82b68e8f95b3d0d614baa1a1564fb215ecf1b9
Mohammed Yahya <yahyamd97@gmail.com>
2025-05-11 21:59:07 +0530

    changes

--- utils/uploadToS3 .js
+++ utils/uploadToS3 .js
@@ -1,12 +1,15 @@
 const multer = require('multer');
 const multerS3 = require('multer-s3');
 const { s3 } = require('../utils/aw-config');
+require('dotenv').config();
+
 
 const uploadToS3 = (bucketName) => {
+    console.log(bucketName)
     return multer({
         storage: multerS3({
             s3: s3,
-            bucket: bucketName,
+            bucket: process.env.S3_BUCKET_NAME,
             metadata: (req, file, cb) => {
                 cb(null, { fieldName: file.fieldname });
             },
@@ -19,8 +22,9 @@ const uploadToS3 = (bucketName) => {
 
 
 const deleteFromS3 = (bucketName, key) => {
+    console.log(bucketName)
     const params = {
-        Bucket: bucketName,
+        Bucket: process.env.S3_BUCKET_NAME,
         Key: key
     }
 
